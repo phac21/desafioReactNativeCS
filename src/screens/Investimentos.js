981fed0a4bb385bbf72b02a19b7215bf30d4970c
Pedro Henrique <pedro.semear@gmail.com>
2022-02-01 12:09:18 -0300

    :sparkles: Cria telas de investimentos e resgate

---
+++ src/screens/Investimentos.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+import { View, Text, StyleSheet, Platform, FlatList} from "react-native";
+
+export default function Investimentos(){
+    return(
+        <View>
+            <Text>Desafio React Native CooperSystem</Text>
+        </View>
+    )
+}
